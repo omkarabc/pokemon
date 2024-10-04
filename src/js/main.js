33752ed718a79f972499a5d84aae0c5e3acd9f78
omkarabc <153627039+omkarabc@users.noreply.github.com>
2024-10-04 08:58:02 +0000

    update

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+// === DOM Selection ===
+const inputEl = document.querySelector('input');
+
+// Add / to focus input
+document.addEventListener('keypress', function () {
+  console.log('waah');
+});

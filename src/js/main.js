58a5af4ddc92fb14c998305d325c6d0d1f99e445
omkarabc <153627039+omkarabc@users.noreply.github.com>
2024-10-04 09:35:44 +0000

    complete

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,38 @@
+import data from './data.json';
+
 // === DOM Selection ===
 const inputEl = document.querySelector('input');
+const rowEl = document.querySelector('#row');
+
+// show pokemon name in paragraph inside the row
+for (let pokemonObj of data) {
+  const div = document.createElement('div');
+  div.classList.add('col');
+
+  div.innerHTML = `
+  <div class="card">
+    <img
+      src="${pokemonObj.image}"
+      class="card-img-top"
+      alt="${pokemonObj.name}"
+    />
+    <div class="card-body">
+      <h5 class="card-title">${pokemonObj.name}</h5>
+      <p class="card-text">
+        ${pokemonObj.description}
+      </p>
+    </div>
+  </div>
+  `;
+
+  rowEl.appendChild(div);
+}
 
 // Add / to focus input
-document.addEventListener('keypress', function () {
-  console.log('waah');
+document.addEventListener('keypress', function (event) {
+  if (event.key === '/') {
+    event.preventDefault(); // esko (/) ko likhna nhi hai
+    inputEl.focus();
+  }
 });
+
